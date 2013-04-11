c265b55cd931e07970fe306b49c933a0b962108b
Noah Adler <noah.adler@uky.edu>
2013-04-11 15:57:01 -0400

    Move taxonomy term reference into a template in the module, rather than the theme

---
+++ templates/field--taxonomy-term-reference.tpl.php
@@ -0,0 +1,20 @@
+<?php
+$output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<h3 class="field-label">' . $variables['label'] . ': </h3>';
+  }
+
+  // Render the items.
+  $output .= ($variables['element']['#label_display'] == 'inline') ? '<ul class="links inline">' : '<ul class="links">';
+  foreach ($variables['items'] as $delta => $item) {
+    $output .= '<li class="taxonomy-term-reference-' . $delta . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</li>';
+  }
+  $output .= '</ul>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . (!in_array('clearfix', $variables['classes_array']) ? ' clearfix' : '') . '">' . $output . '</div>';
+
+  print $output;
+?>
