88d2f9f3b19328b62ea4763c43fba097fef9f008
Noah Adler <noah.adler@uky.edu>
2013-04-12 13:39:45 -0400

    Add popup for tags showing description and ability to remove tags

--- templates/field--taxonomy-term-reference.tpl.php
+++ templates/field--taxonomy-term-reference.tpl.php
@@ -1,20 +1,24 @@
+<div class="<?php print $variables['classes'] . (!in_array('clearfix', $variables['classes_array']) ? ' clearfix' : ''); ?>">
 <?php
-$output = '';
-
   // Render the label, if it's not hidden.
-  if (!$variables['label_hidden']) {
-    $output .= '<h3 class="field-label">' . $variables['label'] . ': </h3>';
-  }
-
+  if (!$variables['label_hidden']): ?>
+    <h3 class="field-label">' <?php print $variables['label']; ?>: </h3>
+<?php endif;
   // Render the items.
-  $output .= ($variables['element']['#label_display'] == 'inline') ? '<ul class="links inline">' : '<ul class="links">';
-  foreach ($variables['items'] as $delta => $item) {
-    $output .= '<li class="taxonomy-term-reference-' . $delta . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</li>';
-  }
-  $output .= '</ul>';
-
-  // Render the top-level DIV.
-  $output = '<div class="' . $variables['classes'] . (!in_array('clearfix', $variables['classes_array']) ? ' clearfix' : '') . '">' . $output . '</div>';
-
-  print $output;
-?>
+  print ($variables['element']['#label_display'] == 'inline') ? '<ul class="links inline">' : '<ul class="links">';
+  foreach ($variables['items'] as $delta => $item): ?>
+    <li class="taxonomy-term-reference-<?php print $delta; ?>"<?php print $variables['item_attributes'][$delta]; ?>>
+      <?php print drupal_render($item); ?>
+      <div class="tag-term-info" data-tid="<?php print $item['#options']['entity']->tid; ?>" style="display:none;">
+        <?php
+          $term = taxonomy_term_load($item['#options']['entity']->tid);
+          dpm($term);
+        ?>
+        <strong><?php print $term->name; ?></strong>
+        <div class="tag-term-description"><?php print $term->description; ?></div>
+        <div class="remove-tag-wrapper"><a class="remove-tag">Remove</a></div>
+      </div>
+    </li>
+<?php endforeach; ?>
+</ul>
+</div>
